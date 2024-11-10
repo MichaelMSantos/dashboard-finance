e46bbb833c3ac281eda628e00bea44a355fc7ed7
Michael M Santos <mmss240610@gmail.com>
2024-11-10 08:58:35 -0300

    feat: ensure transaction can be created

---
+++ app/_actions/add-transaction/schema.ts
@@ -0,0 +1,17 @@
+import {
+  TransactionCategory,
+  TransactionpPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
+import { z } from "zod";
+
+export const addTransactionSchema = z.object({
+  name: z.string().trim().min(1),
+  amount: z.number().positive(),
+
+  type: z.nativeEnum(TransactionType),
+
+  category: z.nativeEnum(TransactionCategory),
+  paymentMethod: z.nativeEnum(TransactionpPaymentMethod),
+  date: z.date(),
+});
