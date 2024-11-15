d571d0858bc506604e085f2336945e31ebe59ae0
Michael M Santos <mmss240610@gmail.com>
2024-11-14 22:46:46 -0300

    Update index.ts

--- app/subscription/_actions/create-stripe-checkout/index.ts
+++ app/subscription/_actions/create-stripe-checkout/index.ts
@@ -2,25 +2,23 @@
 
 import { auth } from "@clerk/nextjs/server";
 import Stripe from "stripe";
+
 export const createStripeCheckout = async () => {
   const { userId } = await auth();
   if (!userId) {
     throw new Error("Unauthorized");
   }
-
   if (!process.env.STRIPE_SECRET_KEY) {
-    throw new Error("Stripe secret key not found!");
+    throw new Error("Stripe secret key not found");
   }
-
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2024-10-28.acacia",
   });
-
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     mode: "subscription",
-    success_url: "https://dashboard-finance-4zko.vercel.app",
-    cancel_url: "https://dashboard-finance-4zko.vercel.app",
+    success_url: process.env.APP_URL,
+    cancel_url: process.env.APP_URL,
     subscription_data: {
       metadata: {
         clerk_user_id: userId,
